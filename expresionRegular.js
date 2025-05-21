429300b512304ee09c170ed7ca5f6d82a855cd17
l4mole <danielrojo9822@gmail.com>
2025-05-20 23:37:55 -0500

    Add PIN validation function with test cases

---
+++ expresionRegular.js
@@ -0,0 +1,18 @@
+function validatePIN(pin) {
+    return /^(\d{4}|\d{6})$/.test(pin)
+}
+
+// Test
+console.log(validatePIN('1234'))
+console.log(validatePIN('12345'))
+console.log(validatePIN('123456'))
+console.log(validatePIN('a234'))
+console.log(validatePIN(''))
+console.log(validatePIN('1'))
+console.log(validatePIN('12'))
+console.log(validatePIN('123'))
+console.log(validatePIN('1234567'))
+console.log(validatePIN('-1234'))
+console.log(validatePIN('1.234'))
+console.log(validatePIN('0000'))
+console.log(validatePIN('000000'))
